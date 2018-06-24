a705bcdc9aa73a4c0ce71e019ee8771b55ff3e92
Steven Wang <webmaster@silverwzw.com>
2018-06-24 01:41:22 -0400

    Provide different declaration file for non-module project

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Typescript declaration file for Tampermonkey
+# Typescript declaration for Tampermonkey
+
+- tampermonkey-reference.d.ts:
+    Use triple-slash directives to include the declareations in this file
+
+- tampermonkey-module.d.ts:
+    'import' to use tempermonkey api as an external module
 
 # License
 Apache License
--- tampermonkey-reference.d.ts
+++ tampermonkey-reference.d.ts
@@ -1,4 +1,5 @@
 declare var unsafeWindow : Window;
+
 declare var GM_info : {
     version          : string,
     scriptWillUpdate : boolean,
@@ -48,54 +49,67 @@ declare var GM_info : {
     [ key : string ] : any,
 };
 
-export function GM_addStyle(css : string) : void;
+declare function GM_addStyle(css : string) : void;
 
-export function GM_deleteValue(name : string) : void;
+declare function GM_deleteValue(name : string) : void;
 
-export function GM_listValues() : string[];
+declare function GM_listValues() : string[];
+
+declare function GM_addValueChangeListener(name : string, listener : GM_Types.ValueChangeListener) : number;
+
+declare function GM_removeValueChangeListener(listenerId : number) : void;
+
+declare function GM_setValue(name : string, value : any) : void;
+
+declare function GM_getValue(name : string, defaultValue ?: any) : any;
+
+declare function GM_log(message : string) : any;
+
+declare function GM_getResourceText(name : string) : string;
+
+declare function GM_getResourceURL(name : string) : string;
+
+declare function GM_registerMenuCommand(name : string, listener: Function, accessKey ?: string) : number;
+
+declare function GM_unregisterMenuCommand(id : number) : void;
+
+declare function GM_openInTab(url: string, options : GM_Types.OpenTabOptions) : void;
+declare function GM_openInTab(url: string, loadInBackground : boolean) : void;
+declare function GM_openInTab(url: string) : void;
+
+declare function GM_xmlhttpRequest<CONTEXT_TYPE>(details : GM_Types.XHRDetails<CONTEXT_TYPE>) : GM_Types.AbortHandle<void>;
+
+    
+declare function GM_download(details : GM_Types.DownloadDetails) : GM_Types.AbortHandle<boolean>;
+declare function GM_download(url: string, filename: string) : GM_Types.AbortHandle<boolean>;
+
+declare function GM_getTab(callback : (obj : object) => any) : void;
+declare function GM_saveTab(obj : object) : void;
+declare function GM_getTabs(callback : (objs : { [ key : number ] : object }) => any) : void;
+
+declare function GM_notification(details : GM_Types.NotificationDetails, ondone : Function) : void;
+declare function GM_notification(text : string, title : string, image : string, onclick : Function) : void;
+
+declare function GM_setClipboard(data : string, info ?: string | { type ?: string, minetype ?: string}) : void;
 
 declare namespace GM_Types {
+
     type ValueChangeListener = (name : string, oldValue : any, newValue : any, remote : boolean) => any;
-}
-export function GM_addValueChangeListener(name : string, listener : GM_Types.ValueChangeListener) : number;
-
-export function GM_removeValueChangeListener(listenerId : number) : void;
-
-export function GM_setValue(name : string, value : any) : void;
-
-export function GM_getValue(name : string, defaultValue : any) : any;
-
-export function GM_log(message : string) : any;
-
-export function GM_getResourceText(name : string) : string;
-
-export function GM_getResourceURL(name : string) : string;
-
-export function GM_registerMenuCommand(name : string, listener: Function, accessKey ?: string) : number;
-
-export function GM_unregisterMenuCommand(id : number) : void;
-
-declare namespace GM_Types {
+    
     interface OpenTabOptions {
         active    ?: boolean,
         insert    ?: boolean,
         setParent ?: boolean
     }
-}
-export function GM_openInTab(url: string, options : GM_Types.OpenTabOptions) : void;
-export function GM_openInTab(url: string, loadInBackground : boolean) : void;
-export function GM_openInTab(url: string) : void;
-
-declare namespace GM_Types {
-
+    
     interface XHRResponse<CONTEXT_TYPE> extends Function {
-
+    
         DONE             : 4,
         HEADERS_RECEIVED : 2,
         LOADING          : 3,
         OPENED           : 1,
         UNSENT           : 0
-
+    
         context         : CONTEXT_TYPE,
         finalUrl        : string,
         readyState      : 0 | 1 | 2 | 3 | 4,
@@ -115,9 +129,9 @@ declare namespace GM_Types {
         total            : number,
         totalSize        : number
     }
-
+    
     type Listener<OBJ> =  (this : OBJ, event : OBJ) => any;
-
+    
     interface XHRDetails<CONTEXT_TYPE> {
         method           ?: "GET" | "HEAD" | "POST",
         url              ?: string,
@@ -132,7 +146,7 @@ declare namespace GM_Types {
         fetch            ?: boolean,
         username         ?: string,
         password         ?: string,
-
+    
         onload             ?: Listener<XHRResponse<CONTEXT_TYPE>>,
         onloadstart        ?: Listener<XHRResponse<CONTEXT_TYPE>>,
         onprogress         ?: Listener<XHRProgress<CONTEXT_TYPE>>,
@@ -141,20 +155,16 @@ declare namespace GM_Types {
         onabort            ?: Function,
         onerror            ?: Function
     }
-
+    
     interface AbortHandle<RETURN_TYPE> {
         abort() : RETURN_TYPE
     }
-}
-export function GM_xmlhttpRequest<CONTEXT_TYPE>(details : GM_Types.XHRDetails<CONTEXT_TYPE>) : GM_Types.AbortHandle<void>;
-
-declare namespace GM_Types {
-
+    
     interface DownloadError {
         error    : "not_enabled" | "not_whitelisted" | "not_permitted" | "not_supported" | "not_succeeded",
         details ?: string
     }
-
+    
     interface DownloadDetails {
         url         : string,
         name        : string,
@@ -166,23 +176,14 @@ declare namespace GM_Types {
         onload     ?: Listener<object>,
         onprogress ?: Listener<XHRProgress<void>>
     }
-}
-export function GM_download(details : GM_Types.DownloadDetails) : GM_Types.AbortHandle<boolean>;
-export function GM_download(url: string, filename: string) : GM_Types.AbortHandle<boolean>;
-
-export function GM_getTab(callback : (obj : object) => any) : void;
-export function GM_saveTab(obj : object) : void;
-export function GM_getTabs(callback : (objs : { [ key : number ] : object }) => any) : void;
-
-declare namespace GM_Types {
     
     interface NotificationThis extends NotificationDetails {
         id : string
     }
-
+    
     type NotificationOnClick = (this : NotificationThis) => any;
     type NotificationOnDone = (this : NotificationThis, clicked: boolean) => any;
-
+    
     interface NotificationDetails {
         text      ?: string,
         title     ?: string,
@@ -193,7 +194,3 @@ declare namespace GM_Types {
         ondone    ?: NotificationOnDone,
     }
 }
-export function GM_notification(details : GM_Types.NotificationDetails, ondone : Function) : void;
-export function GM_notification(text : string, title : string, image : string, onclick : Function) : void;
-
-export function GM_setClipboard(data : string, info ?: string | { type ?: string, minetype ?: string}) : void;
